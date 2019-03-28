144f39df24ef68a1d75c3947683a05acf7c0b5af
Matthew Schmidt <47067179+matts0809@users.noreply.github.com>
2019-03-27 20:14:47 -0500

    Edits to Archive page

--- archive.php
+++ archive.php
@@ -1,25 +1,21 @@
 <?php get_header();?>
 
-<div class="header-section" style="background-image: url(<?php the_field('header-image'); ?>)">
-  <div class="header-section-info">
-	  <h1 class="h1-header-image">Blog</h1>
-  </div>
-</div>
+<div class="archive-main-content">
 
-<div class="latests-flex">
-	
+<div class="archive-posts">
+
 <?php $i = 1; if ( have_posts() ) : while ( have_posts() && $i < 3) : the_post(); ?>
-	
-<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
-			
-		<div class="latests-rows" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat;">
-			<a href="<?php the_permalink() ;?>">
-			<h3 class="posts-titles"><?php the_title(); ?></h3>
-			<?php the_time();?>
-			<?php the_excerpt(); ?>	
-			</a>
+
+		<div class="archive-posts-columns">
+			<?php the_post_thumbnail('medium-large-thumbnail-size');?>
+			<div class="archive-post-content">
+				<a href="<?php the_permalink() ;?>">
+				<h3 class="archive-post-titles"><?php the_title(); ?></h3></a>
+				<span><?php the_author(); ?></span>
+				<?php the_excerpt(); ?>
+			</div>
 		</div>
-	
+
 <?php $i++; endwhile; ?>
 	<p>
 		<?php next_posts_link(); ?>
@@ -27,11 +23,13 @@
 	<p>
 		<?php previous_posts_link(); ?>
 	</p>
-	
+
 <?php else : ?>
 	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
-	
+
+</div>
+
 </div>
 
 <?php get_footer();?>
